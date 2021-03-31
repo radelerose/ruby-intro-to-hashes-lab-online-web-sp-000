634632e57642588358dc4b9050af318e016c7b32
Radele Rose <radele.rose@gmail.com>
2021-03-31 03:32:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -62,11 +62,11 @@ def monopoly_with_fourth_tier
 				},
 				:b_and_o_railroad=>{
 					"mortgage_value"=>"$400"
-				},
+				}
 				:shortline=>{
 					"mortgage_value"=>"$800"
 				}
-			},
+			}
 			:rent_in_dollars=>{
 				:one_piece_owned=>25,
 				:two_pieces_owned=>50,
